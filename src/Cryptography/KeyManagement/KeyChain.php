6686bc92090a2996b4a2188b555c7d5e4f317231
Andy Snell <andy@phoneburner.com>
2025-05-07 11:36:19 -0500

    --wip-- [skip ci]

--- src/Cryptography/KeyManagement/KeyChain.php
+++ src/Cryptography/KeyManagement/KeyChain.php
@@ -7,7 +7,11 @@ namespace PhoneBurner\SaltLite\Cryptography\KeyManagement;
 use PhoneBurner\SaltLite\Collections\Map\GenericMapCollection;
 use PhoneBurner\SaltLite\Cryptography\Asymmetric\EncryptionKeyPair;
 use PhoneBurner\SaltLite\Cryptography\Asymmetric\SignatureKeyPair;
+use PhoneBurner\SaltLite\Cryptography\Asymmetric\SignaturePublicKey;
+use PhoneBurner\SaltLite\Cryptography\Paseto\Paserk;
 use PhoneBurner\SaltLite\Cryptography\Symmetric\SharedKey;
+use PhoneBurner\SaltLite\Cryptography\Util;
+use PhoneBurner\SaltLite\String\BinaryString\BinaryString;
 
 /**
  * Keeps track of keys derived from the app key, for use in encryption and decryption.
@@ -25,6 +29,8 @@ use PhoneBurner\SaltLite\Cryptography\Symmetric\SharedKey;
  */
 final class KeyChain extends GenericMapCollection
 {
+    private array $signature_keys = [];
+
     public function __construct(
         public readonly SharedKey $app_key,
         private EncryptionKeyPair|null $encryption_key_pair = null,
@@ -49,6 +55,40 @@ final class KeyChain extends GenericMapCollection
     {
         return $this->signature_key_pair ??= KeyDerivation::signature($this->app_key);
     }
+
+    public function addSignatureKey(string $context, SignatureKeyPair|SignaturePublicKey $key): void
+    {
+        // If we already have the keypair in the collection, don't replace it with a public key
+        $existing = $this->signature_keys[$context] ?? null;
+        if ($existing instanceof SignatureKeyPair && $key instanceof SignaturePublicKey) {
+            return;
+        }
+
+        // index by context, the public key material, and paserk
+        $this->signature_keys[$context] = $key;
+        $this->signature_keys[$key->public->bytes()] = $key;
+        $this->signature_keys[(string)Paserk::pid($key)] = $key;
+
+        if ($key instanceof SignatureKeyPair) {
+            $this->signature_keys[(string)Paserk::sid($key)] = $key;
+        }
+    }
+
+    public function findSignaturePublicKey(Paserk|BinaryString|string $context): SignaturePublicKey|null
+    {
+        $context = Util::bytes($context);
+        $key = $this->signature_keys[Util::bytes($context)] ?? null;
+        if ($key instanceof SignatureKeyPair) {
+            return $key->public;
+        }
+
+        if ($key instanceof SignaturePublicKey) {
+            return $key;
+        }
+
+        return null;
+    }
+
 
     #[\Override]
     public function clear(): void
